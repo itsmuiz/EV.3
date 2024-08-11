354e2f749fb184363c1fa245922d82819f3e92f1
itsmuiz <pvt.muiz1012@gmail.com>
2024-08-11 20:52:54 +0530

    wa.me

--- script.js
+++ script.js
@@ -28,3 +28,13 @@ const header = document.querySelector('header');
 hamburger.addEventListener('click', () => {
     header.classList.toggle('nav-open');
 });
+
+
+function sendWhatsAppMessage(productName, productPrice, productDetails) {
+    const phoneNumber = '918750704927'; // Replace with your WhatsApp number in international format without the plus sign
+    const message = `Hello, I would like to add the following product to my cart:\n\nProduct: ${productName}\nPrice: ${productPrice}\nDetails: ${productDetails}`;
+    
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+  }
+  
